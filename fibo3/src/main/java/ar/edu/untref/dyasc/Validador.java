a278559f7baecc53864d46f5f4eb81ba1c828fd5
Ezequiel <ezequiel.ledesma026@gmail.com>
2018-09-15 12:57:49 -0300

    update

--- fibo3/src/main/java/ar/edu/untref/dyasc/Validador.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Validador.java
@@ -2,19 +2,7 @@ package ar.edu.untref.dyasc;
 
 public class Validador {
 
-    /*
-     * private final static String OPTION_VD = "-o=vd"; private final static String
-     * OPTION_VI = "-o=vi"; private final static String OPTION_HD = "-o=hd"; private
-     * final static String OPTION_HI = "-o=hi";
-     * 
-     * public Boolean chequeoArgumento(String[] arguments) { if (arguments.length ==
-     * 2) { return chequeoPrimerArgumento(arguments[0]); } return arguments.length
-     * <= 4; }
-     * 
-     * private Boolean chequeoPrimerArgumento(String firstArgument) { return
-     * (firstArgument.equals(OPTION_VD) || firstArgument.equals(OPTION_VI) ||
-     * firstArgument.equals(OPTION_HD) || firstArgument.equals(OPTION_HI)); }
-     */
+
     private final static String[] validaArgumento = new String[] { "-o=[vhp][di]", "-m=[ls]", "-f=[^\\s]+" };
 
     public Boolean chequeoArgumento(String[] arguments) {
